dd24a035d7d07ed8709c5a8cce0b8c1f85a74194
Kwak jeong Hun <kjh0520kjh@gmail.com>
2026-03-30 19:54:05 +0900

    Add 0325 my-app project

---
+++ 0325/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Clock from "./Clock";
+
+function App() {
+  return (
+    <div>
+      <Clock /> 
+    </div>
+  );
+}
+
+export default App;
+// 외부에서 사용할 수있게 하는 기능
